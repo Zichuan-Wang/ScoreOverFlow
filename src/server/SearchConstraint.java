71ae1d938b1d3bf224161028f994b1554f6d6f46
TCXX <tiancai_xinxin@126.com>
2018-10-27 00:19:30 -0400

    Finished the SearchConstraint class

--- src/server/SearchConstraint.java
+++ src/server/SearchConstraint.java
@@ -8,9 +8,58 @@ public class SearchConstraint {
 	Time startTime;
 	Time endTime;
 	String roomName;
-	int Capacity;
+	int capacity;
 	
 	public SearchConstraint() {
-		// TODO(Cindy): implement this.
+		this.eventDate = new Date(0);
+		this.startTime = new Time(0);
+		this.endTime = new Time(0);
+		roomName = "";
+		this.capacity = 0;
+	}
+	
+	public Date getEventDate() {
+		return eventDate;
+	}
+	
+	public SearchConstraint setEventDate(Date eventDate) {
+		this.eventDate = eventDate;
+		return this;
+	}
+	
+	public Time getStartTime() {
+		return startTime;
+	}
+	
+	public SearchConstraint setStartTime(Time startTime) {
+		this.startTime = startTime;
+		return this;
+	}
+	
+	public Time getEndTime() {
+		return endTime;
+	}
+	
+	public SearchConstraint setEndTime(Time endTime) {
+		this.endTime = endTime;
+		return this;
+	}
+	
+	public String getRoomName() {
+		return this.roomName;
+	}
+	
+	public SearchConstraint setRoomName(String roomName) {
+		this.roomName = roomName;
+		return this;
+	}
+	
+	public int getCapacity() {
+		return this.capacity;
+	}
+	
+	public SearchConstraint setCapacity(int capacity) {
+		this.capacity = capacity;
+		return this;
 	}
 }
---
+++ src/test/SearchConstraintTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import server.SearchConstraint;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class SearchConstraintTest {
+	private static final Date DEFAULT_EVENT_DATE = new Date(1234567);
+	private static final Time DEFAULT_START_TIME = new Time(12345);
+	private static final Time DEFAULT_END_TIME = new Time(54321);
+	private static final String DEFAULT_ROOM_NAME = "***";
+	private static final int DEFAULT_CAPACITY = 10;
+	
+	@Test
+	public void createAndChangeSearchConstraint() {
+		SearchConstraint constraint = new SearchConstraint();
+		assertNotNull(constraint);
+		
+		constraint.setCapacity(DEFAULT_CAPACITY)
+		.setStartTime(DEFAULT_START_TIME)
+		.setEndTime(DEFAULT_END_TIME)
+		.setRoomName(DEFAULT_ROOM_NAME)
+		.setEventDate(DEFAULT_EVENT_DATE);
+		
+		assertEquals(constraint.getCapacity(), DEFAULT_CAPACITY);
+		assertEquals(constraint.getStartTime(), DEFAULT_START_TIME);
+		assertEquals(constraint.getEndTime(), DEFAULT_END_TIME);
+		assertEquals(constraint.getRoomName(), DEFAULT_ROOM_NAME);
+		assertEquals(constraint.getEventDate(), DEFAULT_EVENT_DATE);
+	}
+}
