f7047fead911b4cd892285c93d5bc1f048fd803b
TCXX <tiancai_xinxin@126.com>
2018-10-27 11:32:26 -0400

    Added the SearchReservationConstraint class

---
+++ src/test/SearchReservationConstraintTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import server.SearchReservationConstraint;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SearchReservationConstraintTest {
+	private static final int DEFAULT_USER_ID = 1;
+	
+	@Test
+	public void createAndChangeSearchConstraint() {
+		SearchReservationConstraint constraint = new SearchReservationConstraint();
+		assertNotNull(constraint);
+		
+		constraint.setUserId(DEFAULT_USER_ID);
+		
+		assertEquals(constraint.getUserId(), DEFAULT_USER_ID);
+	}
+	
+}
