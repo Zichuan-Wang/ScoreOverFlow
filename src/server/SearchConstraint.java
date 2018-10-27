670ef3148500818294742c6bfbe5d193d5538a38
TCXX <tiancai_xinxin@126.com>
2018-10-26 23:54:49 -0400

    Created the SearchConstraint class

---
+++ src/server/SearchConstraint.java
@@ -0,0 +1,16 @@
+package server;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class SearchConstraint {
+	Date eventDate;
+	Time startTime;
+	Time endTime;
+	String roomName;
+	int Capacity;
+	
+	public SearchConstraint() {
+		// TODO(Cindy): implement this.
+	}
+}
