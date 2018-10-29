9639a349621913a6f9cde5eec2758c373beae6d2
HenryX <hx2209@columbia.edu>
2018-10-29 01:35:21 -0400

    using factory

--- src/main/server/action/ReserveRoomAction.java
+++ src/main/server/action/ReserveRoomAction.java
@@ -3,12 +3,13 @@ package server.action;
 import java.sql.Timestamp;
 
 import dao.ReservationDao;
+import dao.factory.ReservationDaoFactory;
 import entity.Reservation;
 import exception.DBConnectionException;
 
 public class ReserveRoomAction {
 	public static boolean reserveRoom(Reservation reservation) throws DBConnectionException {
-		ReservationDao reservationDao = new ReservationDao();
+		ReservationDao reservationDao = ReservationDaoFactory.getInstance();
 		reservation.setCreated(new Timestamp(System.currentTimeMillis()));
 		reservation.setModified(new Timestamp(System.currentTimeMillis()));
 		reservationDao.saveOrUpdate(reservation);
