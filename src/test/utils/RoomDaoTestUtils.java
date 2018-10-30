565c5a14a392f08914b6f4598cef7db4ce31e648
HenryX <hx2209@columbia.edu>
2018-10-30 17:02:21 -0400

    cleaning up

--- src/test/utils/RoomDaoTestUtils.java
+++ src/test/utils/RoomDaoTestUtils.java
@@ -2,13 +2,12 @@ package utils;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
