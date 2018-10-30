e27e76ce7bbc5c5aa40f3c2cd89a8ea8d5741ee6
Zichuan "Max" Wang <zichuan.wang@columbia.edu>
2018-10-30 13:16:57 -0400

    Update push.sh

--- push.sh
+++ push.sh
@@ -13,15 +13,17 @@ commit_logs() {
   cat maven_log
   cat **/*PMD.html
   cat *PMD.html
+  find . -name *PMD* -print0 | xargs -0 -n1 dirname | sort --unique
+  find . -name **/*PMD* -print0 | xargs -0 -n1 dirname | sort --unique
   git checkout -b master
   git add maven_log
-  git commit --message "Azure CI log ***NO_CI***"
+  git commit -m "Azure CI log ***NO_CI***"
 }
 
 
 
 upload_files() {
-  git remote set-url origin https://Zichuan-Wang:9cade35bf393291c9ac26f3397fdf232437c7d14@github.com/HengruiX/ScoreOverFlow.git
+  git remote set-url origin https://Zichuan-Wang:1e8af7f8ec67794a6e41d8b90123f15da0e361da@github.com/HengruiX/ScoreOverFlow.git
   git push --quiet --set-upstream origin master
 }
 
