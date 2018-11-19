1b06a7ea046339e1494d12141d6349569d33d01e
HenryX <hx2209@columbia.edu>
2018-11-19 18:20:44 -0500

    implementing mail sender

---
+++ src/main/email/EmailSender.java
@@ -0,0 +1,45 @@
+package email;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailSender {
+
+	public static void sendEmail(String to, String subject, String body) {
+		InputStream inputStream;
+		try {
+			inputStream = new FileInputStream("src/main/resources/email.properties");
+			Properties properties = new Properties();
+			properties.load(inputStream);
+
+			String username = properties.getProperty("mail.from.username");
+			String password = properties.getProperty("mail.from.password");
+			String from = properties.getProperty("mail.from.address");
+
+			Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			message.setSubject(subject);
+			message.setText(body);
+			Transport.send(message);
+
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+}
