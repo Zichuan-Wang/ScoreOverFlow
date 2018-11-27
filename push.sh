5add01d0515753093d07016842ecbc24e52c43fe
Zichuan "Max" Wang <zichuan.wang@columbia.edu>
2018-11-27 11:38:54 -0500

    Update push.sh

--- push.sh
+++ push.sh
@@ -13,11 +13,13 @@ commit_logs() {
   mv ./target/surefire-reports/*.txt reports/test
   mv ./target/cpd.xml reports/pmd
   mv ./target/pmd.xml reports/pmd
+  mv ./target/site/jacoco/* reports/jacoco
   mv maven_phase1.log reports/maven
   mv maven_phase2.log reports/maven
   git add reports/pmd/cpd.xml
   git add reports/pmd/pmd.xml
   git add reports/test/*.txt
+  git add reports/jacoco/*
   git add reports/maven/maven_phase1.log
   git add reports/maven/maven_phase2.log
   git commit -m "Travis CI log [skip ci]"
