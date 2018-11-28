1b57466e2c2443ab9de19b4d22f4e4047045103c
HenryX <hx2209@columbia.edu>
2018-11-28 02:32:46 -0500

    modifying room test

--- src/test/server/action/RoomActionTest.java
+++ src/test/server/action/RoomActionTest.java
@@ -3,11 +3,11 @@ package server.action;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import dao.RoomDao;
 import dao.factory.RoomDaoFactory;
 import entity.Room;
@@ -20,38 +20,37 @@ public class RoomActionTest {
 	public void findRoomById_roomExists() throws DBConnectionException {
 		RoomDao dao = RoomDaoFactory.getInstance();
 		RoomAction action = new RoomAction(dao);
-		
+
 		Room room = dao.saveOrUpdate(EntityTestUtils.getDefaultRoom());
+		Room found = action.getRoomById(EntityTestUtils.DEFAULT_ROOM_ID);
 		dao.remove(room);
-		
-		assertNotNull(action.getRoomById(EntityTestUtils.DEFAULT_ROOM_ID));
-		assertEquals(EntityTestUtils.DEFAULT_UNI, action.getRoomById(EntityTestUtils.DEFAULT_ROOM_ID).getId());
+
+		assertNotNull(found);
+		assertEquals(EntityTestUtils.DEFAULT_ROOM_ID, found.getId());
 	}
-	
+
 	@Test
 	public void findRoomById_roomNotExist() throws DBConnectionException {
 		RoomDao dao = RoomDaoFactory.getInstance();
 		RoomAction action = new RoomAction(dao);
-		
+
 		assertNull(action.getRoomById(EntityTestUtils.DEFAULT_ROOM_ID));
 	}
-	
+
 	@Test
 	public void searchRooms_capacity() throws DBConnectionException {
 		RoomDao dao = RoomDaoFactory.getInstance();
 		RoomAction action = new RoomAction(dao);
-		
+
 		Room room = dao.saveOrUpdate(EntityTestUtils.getDefaultRoom());
+		SearchRoomConstraint constraint = new SearchRoomConstraint();
+		List<Room> results1 = action.searchRooms(constraint.setCapacity(EntityTestUtils.DEFAULT_CAPACITY));
+		List<Room> results2 = action.searchRooms(constraint.setCapacity(EntityTestUtils.DEFAULT_CAPACITY + 1));
+		List<Room> results3 = action.searchRooms(constraint.setCapacity(EntityTestUtils.DEFAULT_CAPACITY - 1));
 		dao.remove(room);
-		
-		SearchRoomConstraint constraint = new SearchRoomConstraint()
-				.setCapacity(EntityTestUtils.DEFAULT_CAPACITY);
-		assertEquals(constraint.getCapacity(), EntityTestUtils.DEFAULT_CAPACITY);
-		
-		List<Room> results = action.searchRooms(constraint);
-		assertNotNull(results);
-		assertEquals(results.size(), 1);
-		assertNotNull(results.get(0));
-		assertTrue(results.get(0).getCapacity() >= EntityTestUtils.DEFAULT_CAPACITY);
+
+		assertEquals(results1.size(), 1);
+		assertEquals(results2.size(), 0);
+		assertEquals(results3.size(), 1);
 	}
 }
