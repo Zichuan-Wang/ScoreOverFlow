32eab19925beca13a7b16f0365a61d25160ce5ac
TCXX <tiancai_xinxin@126.com>
2018-11-28 03:10:58 -0500

    Update RoomActionTest.java

--- src/test/server/action/RoomActionTest.java
+++ src/test/server/action/RoomActionTest.java
@@ -50,7 +50,24 @@ public class RoomActionTest {
 		dao.remove(room);
 
 		assertEquals(results1.size(), 1);
+		assertEquals(results1.get(0).getCapacity(), EntityTestUtils.DEFAULT_CAPACITY);
 		assertEquals(results2.size(), 0);
 		assertEquals(results3.size(), 1);
 	}
+	
+	@Test
+	public void searchRooms_name() throws DBConnectionException {
+		RoomDao dao = RoomDaoFactory.getInstance();
+		RoomAction action = new RoomAction(dao);
+
+		Room room = dao.saveOrUpdate(EntityTestUtils.getDefaultRoom());
+		SearchRoomConstraint constraint = new SearchRoomConstraint();
+		List<Room> results1 = action.searchRooms(constraint.setRoomName(EntityTestUtils.DEFAULT_ROOM_NAME));
+		List<Room> results2 = action.searchRooms(constraint.setRoomName(EntityTestUtils.DEFAULT_ROOM_NAME + "=")); // non-exist
+		dao.remove(room);
+
+		assertEquals(results1.size(), 1);
+		assertEquals(results1.get(0).getName(), EntityTestUtils.DEFAULT_ROOM_NAME);
+		assertEquals(results2.size(), 0);
+	}	
 }
