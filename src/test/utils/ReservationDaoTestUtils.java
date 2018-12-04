41ffb90f046d35d0b3bc960a2daeeda0ae0d1ec7
HenryX <hx2209@columbia.edu>
2018-12-04 02:06:58 -0500

    cleaning

--- src/test/utils/ReservationDaoTestUtils.java
+++ src/test/utils/ReservationDaoTestUtils.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
