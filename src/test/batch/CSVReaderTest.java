e4a4274ae1d5035fe58d97846218c9a9397a3ac0
HenryX <hx2209@columbia.edu>
2018-12-14 17:55:41 -0500

    modify test

--- src/test/batch/CSVReaderTest.java
+++ src/test/batch/CSVReaderTest.java
@@ -15,15 +15,19 @@ import utils.FacilityActionTestUtils;
 
 public class CSVReaderTest {
 	
-	private static final String CORRECT = "\n\n12/22/2018,15:00,22:10,250,Lerner,projector";
+	private static final String CORRECT1 = "\n\n12/22/2018,15:00,22:10,250,Lerner,projector";
+	private static final String CORRECT2 = "\n\n12/22/2018,15:00,22:10,250,,";
 	private static final String INCORRECT = "\n\n12/222018,15:00,22:10,250,,";
 
 	@Test
 	public void readCorrectCSV() throws CSVParseException, IOException {
-		BufferedReader reader = new BufferedReader(new StringReader(CORRECT));
+		BufferedReader reader1 = new BufferedReader(new StringReader(CORRECT1));
+		BufferedReader reader2 = new BufferedReader(new StringReader(CORRECT2));
 		CSVReader csvReader = new CSVReader(FacilityActionTestUtils.getFacilityAction());
-		List<SearchRoomConstraint> constraints = csvReader.readCSV(reader);
-		assertEquals(1, constraints.size());
+		List<SearchRoomConstraint> constraints1 = csvReader.readCSV(reader1);
+		List<SearchRoomConstraint> constraints2 = csvReader.readCSV(reader2);
+		assertEquals(1, constraints1.size());
+		assertEquals(1, constraints2.size());
 	}
 	
 	@Test
